d6fe787a7efd1f16509262daf62d928bf8aa8a73
lko6981 <57346445+lko6981@users.noreply.github.com>
2019-12-07 21:24:04 +0900

    Add files via upload

---
+++ client2.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <curses.h>
+#include <termios.h>
+
+#define BUF_SIZE 1024
+
+
+
+
+void error_handling(char *message);
+void printmsg();
+void choicemsg();
+
+int sock;
+char message[BUF_SIZE];
+int str_len = 0;
+
+int i = 0;
+
+
+int main(int argc, char *argv[])
+{
+
+	struct sockaddr_in serv_addr;
+
+	if (argc != 3)
+	{
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock == -1)
+		error_handling("socket() error");
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("connect() error!");
+	else
+	{
+		puts("Connected......wait untill there's two players");
+		while (1)
+		{
+			if ((str_len = read(sock, message, BUF_SIZE - 1)) != 0)
+			{
+				message[str_len] = 0;
+				if (strcmp(message, "ready") == 0)
+				{	
+					break;
+				}
+			}
+			else
+			{
+				close(sock);
+				return 0;
+			}
+		}
+	}
+
+	initscr();
+	clear();
+	move(0, 0);
+	addstr("Game start!");
+	move(1, 0);
+	addstr("Input your name(Q to quit) : ");
+	refresh();
+	getstr(message);
+	clear();
+	if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+	{
+		close(sock);
+		return 0;
+	}
+	write(sock, message, strlen(message)); //send name
+
+	while (1)
+	{
+		
+		printmsg(); //my turn
+		//clear();
+	//	choicemsg();
+		//	clear();
+	}
+	
+	endwin();
+	close(sock);
+	return 0;
+}
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+void printmsg()
+{
+	int count = 0;
+	while (1)
+	{
+		if (count)
+		{
+			refresh();
+    	}
+
+		if ((str_len = read(sock, message, BUF_SIZE - 1)) != 0)
+		{
+			message[str_len] = 0;
+			if (strcmp(message, "end") == 0)
+			{
+				break;
+			}
+			else if (strcmp(message, "quit") == 0)
+			{
+				exit(0);
+			}
+		//	move(i++, 0);
+			
+			addstr(message);
+		//	move(count, 0);
+			refresh();
+
+		}
+		else
+		{
+			close(sock);
+			return;
+		}
+		count++;
+	}
+
+	move(20, 0);
+	addstr("Input your choice :");
+	refresh();
+	getstr(message);
+	write(sock, message, strlen(message)); //send
+	clear();
+	return;
+}
+
+/*void choicemsg()
+{
+	move(20, 0); 
+	addstr("Input your choice :");
+	refresh();
+	getstr(message);
+	write(sock, message, strlen(message)); //send
+	clear();
+}*/
